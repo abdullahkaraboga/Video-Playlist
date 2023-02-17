56d35abf87abe9b2fbf6c0e0217ad70ab38c1102
abdullahkaraboga <abdullahkaraboga@icloud.com>
2023-02-17 15:55:02 +0300

    Initial Commit

---
+++ VideoPlaylist/VideoPlaylistApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlaylistApp.swift
+//  VideoPlaylist
+//
+//  Created by Abdullah Karaboğa on 17.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlaylistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
